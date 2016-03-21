598726754f87ebeb59493f64fec0271737c59680
Sudheesh Katkam <skatkam@maprtech.com>
2016-03-21 15:32:47 -0700

    MD-484: For a prepare statement, add LIMIT(0) on top of SCAN
    Description: Currently prepare statements use LIMIT 0 to get the result schema. Adding LIMIT(0) on top of SCAN causes an early termination of the query.
    
    As of today, this feature is not available on Apache Drill.
    
    Other patches included:
    + MD-834: Disable LIMIT(0) on SCAN for UNION and complex functions
      - Fixes a regression caused by MD-484 i.e. UNION and complex functions need data to produce result schema
      - Create an option "planner.enable_limit0_on_scan", enabled by default
    
    + MD-849: If function is unsupported, don't change the plan
      - Fixes this issue with MD-834

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/handlers/DefaultSqlHandler.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/handlers/DefaultSqlHandler.java
@@ -272,6 +272,9 @@ public class DefaultSqlHandler extends AbstractSqlHandler {
         // If the query contains a limit 0 clause, disable distributed mode since it is overkill for determining schema.
         if (FindLimit0Visitor.containsLimit0(convertedRelNodeWithSum0)) {
           context.getPlannerSettings().forceSingleMode();
+          if (context.getOptions().getOption(ExecConstants.LATE_LIMIT0_OPT)) {
+            return FindLimit0Visitor.addLimitOnTopOfLeafNodes(drillRel);
+          }
         }
 
         return drillRel;
