051fa83e37125c282ca5e4fdaaea588c0d80a6c8
Paul Rogers <progers@maprtech.com>
2016-11-13 00:37:09 -0800

    Split batch group to avoid "hidden" batches

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/xsort/managed/BatchGroup.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/xsort/managed/BatchGroup.java
@@ -42,41 +42,211 @@ import org.apache.hadoop.fs.Path;
 
 import com.google.common.base.Stopwatch;
 
-public class BatchGroup implements VectorAccessible, AutoCloseable {
+/**
+ * Represents a group of batches spilled to disk.
+ * <p>
+ * The batches are defined by a schema which can change over time. When the schema changes,
+ * all existing and new batches are coerced into the new schema. Provides a
+ * uniform way to iterate over records for one or more batches whether
+ * the batches are in memory or on disk.
+ * <p>
+ * The <code>BatchGroup</code> operates in two modes as given by the two
+ * subclasses:
+ * <ul>
+ * <li>Input mode (@link InputBatchGroup): Used to buffer in-memory batches
+ * prior to spilling.</li>
+ * <li>Spill mode (@link SpilledBatchGroup): Holds a "memento" to a set
+ * of batches written to disk. Acts as both a reader and writer for
+ * those batches.</li>
+ */
+
+public abstract class BatchGroup implements VectorAccessible, AutoCloseable {
   static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(BatchGroup.class);
 
-  private VectorContainer currentContainer;
-  private SelectionVector2 sv2;
-  private int pointer = 0;
-  private FSDataInputStream inputStream;
-  private FSDataOutputStream outputStream;
-  private Path path;
-  private FileSystem fs;
-  private BufferAllocator allocator;
-  private int spilledBatches = 0;
-  private OperatorContext context;
-  private BatchSchema schema;
+  /**
+   * The input batch group gathers batches buffered in memory before
+   * spilling. The structure of the data is:
+   * <ul>
+   * <li>Contains a single batch received from the upstream (input)
+   * operator.</li>
+   * <li>Associated selection vector that provides a sorted
+   * indirection to the values in the batch.</li>
+   * </ul>
+   */
 
-  public BatchGroup(VectorContainer container, SelectionVector2 sv2, OperatorContext context) {
-    this.sv2 = sv2;
-    this.currentContainer = container;
-    this.context = context;
-  }
+  public static class InputBatchGroup extends BatchGroup {
+    private SelectionVector2 sv2;
 
-  public BatchGroup(VectorContainer container, FileSystem fs, String path, OperatorContext context) {
-    currentContainer = container;
-    this.fs = fs;
-    this.path = new Path(path);
-    this.allocator = context.getAllocator();
-    this.context = context;
-  }
+    public InputBatchGroup(VectorContainer container, SelectionVector2 sv2, OperatorContext context, long batchSize) {
+      super( container, context, batchSize );
+      this.sv2 = sv2;
+    }
 
-  public SelectionVector2 getSv2() {
-    return sv2;
+    public SelectionVector2 getSv2() {
+      return sv2;
+    }
+
+    @Override
+    public int getRecordCount() {
+      if (sv2 != null) {
+        return sv2.getCount();
+      } else {
+        return super.getRecordCount();
+      }
+    }
+
+    @Override
+    public int getNextIndex() {
+      int val = super.getNextIndex();
+      if ( val == -1 ) {
+        return val;
+      }
+      return sv2.getIndex(val);
+    }
+
+    @Override
+    public void close() throws IOException {
+      super.close( );
+      if (sv2 != null) {
+        sv2.clear();
+      }
+    }
   }
 
   /**
-   * Updates the schema for this batch group. The current as well as any deserialized batches will be coerced to this schema
+   * Holds a set of spilled batches, represented by a file on disk.
+   * Handles reads from, and writes to the spill file. The data structure
+   * is:
+   * <ul>
+   * <li>A pointer to a file that contains serialized batches.</li>
+   * <li>When writing, each batch is appended to the output file.</li>
+   * <li>When reading, iterates over each spilled batch, and for each
+   * of those, each spilled record.</li>
+   * </ul>
+   * <p>
+   * Starts out with no current batch. Defines the current batch to be the
+   * (shell: schema without data) of the last batch spilled to disk.
+   * <p>
+   * When reading, has destructive read-once behavior: closing the
+   * batch (after reading) deletes the underlying spill file.
+   */
+
+  public static class SpilledBatchGroup extends BatchGroup {
+    private FSDataInputStream inputStream;
+    private FSDataOutputStream outputStream;
+    private Path path;
+    private FileSystem fs;
+    private BufferAllocator allocator;
+    private int spilledBatches = 0;
+
+    public SpilledBatchGroup(FileSystem fs, String path, OperatorContext context, long batchSize) {
+      super( null, context, batchSize );
+      this.fs = fs;
+      this.path = new Path(path);
+      this.allocator = context.getAllocator();
+    }
+
+    public void addBatch(VectorContainer newContainer) throws IOException {
+      assert fs != null;
+      assert path != null;
+      if (outputStream == null) {
+        outputStream = fs.create(path);
+      }
+      int recordCount = newContainer.getRecordCount();
+      WritableBatch batch = WritableBatch.getBatchNoHVWrap(recordCount, newContainer, false);
+      VectorAccessibleSerializable outputBatch = new VectorAccessibleSerializable(batch, allocator);
+      Stopwatch watch = Stopwatch.createStarted();
+      outputBatch.writeToStream(outputStream);
+      newContainer.zeroVectors();
+      logger.debug("Took {} us to spill {} records", watch.elapsed(TimeUnit.MICROSECONDS), recordCount);
+      spilledBatches++;
+
+      // Hold onto the husk of the last added container so that we have a
+      // current container when starting to read rows back later.
+
+      currentContainer = newContainer;
+      currentContainer.setRecordCount(0);
+    }
+
+    @Override
+    public int getNextIndex() {
+      if (pointer == getRecordCount()) {
+        if (spilledBatches == 0) {
+          return -1;
+        }
+        try {
+          currentContainer.zeroVectors();
+          getBatch();
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+        pointer = 1;
+        return 0;
+      }
+      return super.getNextIndex();
+    }
+
+    private VectorContainer getBatch() throws IOException {
+      assert fs != null;
+      assert path != null;
+      if (inputStream == null) {
+        inputStream = fs.open(path);
+      }
+      VectorAccessibleSerializable vas = new VectorAccessibleSerializable(allocator);
+      Stopwatch watch = Stopwatch.createStarted();
+      vas.readFromStream(inputStream);
+      VectorContainer c =  vas.get();
+      if (schema != null) {
+        c = SchemaUtil.coerceContainer(c, schema, context);
+      }
+//      logger.debug("Took {} us to read {} records", watch.elapsed(TimeUnit.MICROSECONDS), c.getRecordCount());
+      spilledBatches--;
+      currentContainer.zeroVectors();
+      Iterator<VectorWrapper<?>> wrapperIterator = c.iterator();
+      for (VectorWrapper w : currentContainer) {
+        TransferPair pair = wrapperIterator.next().getValueVector().makeTransferPair(w.getValueVector());
+        pair.transfer();
+      }
+      currentContainer.setRecordCount(c.getRecordCount());
+      c.zeroVectors();
+      return c;
+    }
+
+    @Override
+    public void close() throws IOException {
+      super.close( );
+      closeOutputStream( );
+      if (inputStream != null) {
+        inputStream.close();
+      }
+      if (fs != null && fs.exists(path)) {
+        fs.delete(path, false);
+      }
+    }
+
+    public void closeOutputStream() throws IOException {
+      if (outputStream != null) {
+        outputStream.close();
+        outputStream = null;
+      }
+    }
+  }
+
+  protected VectorContainer currentContainer;
+  protected int pointer = 0;
+  protected OperatorContext context;
+  protected BatchSchema schema;
+  private long batchSize;
+
+  public BatchGroup(VectorContainer container, OperatorContext context, long batchSize) {
+    this.currentContainer = container;
+    this.context = context;
+    this.batchSize = batchSize;
+  }
+
+  /**
+   * Updates the schema for this batch group. The current as well as any
+   * deserialized batches will be coerced to this schema.
    * @param schema
    */
   public void setSchema(BatchSchema schema) {
@@ -84,74 +254,12 @@ public class BatchGroup implements VectorAccessible, AutoCloseable {
     this.schema = schema;
   }
 
-  public void addBatch(VectorContainer newContainer) throws IOException {
-    assert fs != null;
-    assert path != null;
-    if (outputStream == null) {
-      outputStream = fs.create(path);
-    }
-    int recordCount = newContainer.getRecordCount();
-    WritableBatch batch = WritableBatch.getBatchNoHVWrap(recordCount, newContainer, false);
-    VectorAccessibleSerializable outputBatch = new VectorAccessibleSerializable(batch, allocator);
-    Stopwatch watch = Stopwatch.createStarted();
-    outputBatch.writeToStream(outputStream);
-    newContainer.zeroVectors();
-    logger.debug("Took {} us to spill {} records", watch.elapsed(TimeUnit.MICROSECONDS), recordCount);
-    spilledBatches++;
-  }
-
-  private VectorContainer getBatch() throws IOException {
-    assert fs != null;
-    assert path != null;
-    if (inputStream == null) {
-      inputStream = fs.open(path);
-    }
-    VectorAccessibleSerializable vas = new VectorAccessibleSerializable(allocator);
-    Stopwatch watch = Stopwatch.createStarted();
-    vas.readFromStream(inputStream);
-    VectorContainer c =  vas.get();
-    if (schema != null) {
-      c = SchemaUtil.coerceContainer(c, schema, context);
-    }
-//    logger.debug("Took {} us to read {} records", watch.elapsed(TimeUnit.MICROSECONDS), c.getRecordCount());
-    spilledBatches--;
-    currentContainer.zeroVectors();
-    Iterator<VectorWrapper<?>> wrapperIterator = c.iterator();
-    for (VectorWrapper w : currentContainer) {
-      TransferPair pair = wrapperIterator.next().getValueVector().makeTransferPair(w.getValueVector());
-      pair.transfer();
-    }
-    currentContainer.setRecordCount(c.getRecordCount());
-    c.zeroVectors();
-    return c;
-  }
-
   public int getNextIndex() {
-    int val;
     if (pointer == getRecordCount()) {
-      if (spilledBatches == 0) {
-        return -1;
-      }
-      try {
-        currentContainer.zeroVectors();
-        getBatch();
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-      pointer = 1;
-      return 0;
+      return -1;
     }
-    if (sv2 == null) {
-      val = pointer;
-      pointer++;
-      assert val < currentContainer.getRecordCount();
-    } else {
-      val = pointer;
-      pointer++;
-      assert val < currentContainer.getRecordCount();
-      val = sv2.getIndex(val);
-    }
-
+    int val = pointer++;
+    assert val < currentContainer.getRecordCount();
     return val;
   }
 
@@ -162,24 +270,6 @@ public class BatchGroup implements VectorAccessible, AutoCloseable {
   @Override
   public void close() throws IOException {
     currentContainer.zeroVectors();
-    if (sv2 != null) {
-      sv2.clear();
-    }
-    if (outputStream != null) {
-      outputStream.close();
-    }
-    if (inputStream != null) {
-      inputStream.close();
-    }
-    if (fs != null && fs.exists(path)) {
-      fs.delete(path, false);
-    }
-  }
-
-  public void closeOutputStream() throws IOException {
-    if (outputStream != null) {
-      outputStream.close();
-    }
   }
 
   @Override
@@ -199,12 +289,14 @@ public class BatchGroup implements VectorAccessible, AutoCloseable {
 
   @Override
   public int getRecordCount() {
-    if (sv2 != null) {
-      return sv2.getCount();
-    } else {
-      return currentContainer.getRecordCount();
-    }
+    return currentContainer.getRecordCount();
   }
+
+  public int getUnfilteredRecordCount() {
+    return currentContainer.getRecordCount();
+  }
+
+  public long getBatchSize( ) { return batchSize; }
 
   @Override
   public Iterator<VectorWrapper<?>> iterator() {
@@ -220,5 +312,4 @@ public class BatchGroup implements VectorAccessible, AutoCloseable {
   public SelectionVector4 getSelectionVector4() {
     throw new UnsupportedOperationException();
   }
-
 }
