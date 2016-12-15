3f12f9ab1113d3773d6db395139b74bb82228ce9
Paul Rogers <progers@maprtech.com>
2016-12-14 17:27:02 -0800

    First cut at mock data generator

--- exec/java-exec/src/main/java/org/apache/drill/exec/store/mock/MockGroupScanPOP.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/store/mock/MockGroupScanPOP.java
@@ -68,28 +68,19 @@ public class MockGroupScanPOP extends AbstractGroupScan {
    */
   protected final List<MockScanEntry> readEntries;
   private LinkedList<MockScanEntry>[] mappings;
-<<<<<<< e87a6c6d939918fe2e5634cf89d2e25a4e62b6ec
 
   /**
    * Whether this group scan uses a newer "extended" schema definition, or the
    * original (non-extended) definition.
    */
 
-=======
->>>>>>> Added revised mock data generator
   private boolean extended;
 
   @JsonCreator
   public MockGroupScanPOP(@JsonProperty("url") String url,
-<<<<<<< e87a6c6d939918fe2e5634cf89d2e25a4e62b6ec
-      @JsonProperty("extended") Boolean extended,
-      @JsonProperty("entries") List<MockScanEntry> readEntries) {
-    super((String) null);
-=======
                           @JsonProperty("extended") Boolean extended,
                           @JsonProperty("entries") List<MockScanEntry> readEntries) {
     super((String)null);
->>>>>>> Added revised mock data generator
     this.readEntries = readEntries;
     this.url = url;
     this.extended = extended == null ? false : extended;
@@ -109,15 +100,12 @@ public class MockGroupScanPOP extends AbstractGroupScan {
     return readEntries;
   }
 
-<<<<<<< e87a6c6d939918fe2e5634cf89d2e25a4e62b6ec
   /**
    * Describes one simulated file (or block) within the logical file scan
    * described by this group scan. Each block can have a distinct schema to test
    * for schema changes.
    */
 
-=======
->>>>>>> Added revised mock data generator
   public static class MockScanEntry {
 
     private final int records;
