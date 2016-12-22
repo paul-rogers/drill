357ec599666a5b16912ce81d1cb5e78d03468d71
Paul Rogers <progers@maprtech.com>
2016-12-21 17:16:17 -0800

    Fix unit tests

--- exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/xsort/TestExternalSort.java
+++ exec/java-exec/src/test/java/org/apache/drill/exec/physical/impl/xsort/TestExternalSort.java
@@ -79,13 +79,6 @@ public class TestExternalSort extends BaseTestQuery {
     return options;
   }
 
-  private String getOptions(boolean testLegacy) {
-    String options = "alter session set `exec.enable_union_type` = true";
-    options += ";alter session set `" + ExecConstants.EXTERNAL_SORT_DISABLE_MANAGED_OPTION.getOptionName() + "` = " +
-        Boolean.toString(testLegacy);
-    return options;
-  }
-
   @Test
   @Ignore("Schema changes are disabled in external sort")
   public void testNumericAndStringTypesManaged() throws Exception {
