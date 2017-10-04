b74b73b29b3ccb53f67b800b3c858990c046a372
Paul Rogers <progers@maprtech.com>
2017-10-04 11:22:53 -0700

    Cleanup

--- exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/protocol/VectorContainerAccessor.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/physical/impl/protocol/VectorContainerAccessor.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.drill.exec.physical.impl.protocol;
 
 import java.util.Iterator;
@@ -14,13 +31,13 @@ import org.apache.drill.exec.record.selection.SelectionVector4;
 public class VectorContainerAccessor implements BatchAccessor {
 
   public static class ContainerAndSv2Accessor extends VectorContainerAccessor {
-  
+
     private SelectionVector2 sv2;
-  
+
     public void setSelectionVector(SelectionVector2 sv2) {
       this.sv2 = sv2;
     }
-  
+
     @Override
     public SelectionVector2 getSelectionVector2() {
       return sv2;
@@ -28,9 +45,9 @@ public class VectorContainerAccessor implements BatchAccessor {
   }
 
   public static class ContainerAndSv4Accessor extends VectorContainerAccessor {
-  
+
     private SelectionVector4 sv4;
-  
+
     @Override
     public SelectionVector4 getSelectionVector4() {
       return sv4;
