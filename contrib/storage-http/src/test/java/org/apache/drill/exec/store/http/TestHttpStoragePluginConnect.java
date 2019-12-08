3ab0ab417997c099f66390325b9242fa9832f77c
Charles Givre <cgivre@apache.org>
2019-12-07 22:38:03 -0500

    Removed JSON Utility File

--- contrib/storage-http/src/test/java/org/apache/drill/exec/store/http/TestHttpStoragePluginConnect.java
+++ contrib/storage-http/src/test/java/org/apache/drill/exec/store/http/TestHttpStoragePluginConnect.java
@@ -18,8 +18,6 @@
 
 package org.apache.drill.exec.store.http;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.drill.exec.store.http.util.JsonConverter;
 import org.apache.drill.exec.store.http.util.SimpleHttp;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -42,39 +40,5 @@ public class TestHttpStoragePluginConnect {
     SimpleHttp http = new SimpleHttp();
     String result = http.get("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today");
     Assert.assertTrue(result, result.contains("\"status\":\"OK\""));
-  }
-
-  /**
-   * This test verifies that the JSON parser succesfully parses the incoming JSON data.  Response should be:
-   * {
-   *       "results":
-   *       {
-   *         "sunrise":"7:27:02 AM",
-   *         "sunset":"5:05:55 PM",
-   *         "solar_noon":"12:16:28 PM",
-   *         "day_length":"9:38:53",
-   *         "civil_twilight_begin":"6:58:14 AM",
-   *         "civil_twilight_end":"5:34:43 PM",
-   *         "nautical_twilight_begin":"6:25:47 AM",
-   *         "nautical_twilight_end":"6:07:10 PM",
-   *         "astronomical_twilight_begin":"5:54:14 AM",
-   *         "astronomical_twilight_end":"6:38:43 PM"
-   *       },
-   *        "status":"OK"
-   *     }
-   */
-  @Test
-  public void testJSONParsing() {
-    SimpleHttp http = new SimpleHttp();
-    String result = http.get("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today");
-    String key = "results";
-
-    // Make sure there was a successful query
-    Assert.assertTrue(result, result.contains("\"status\":\"OK\""));
-
-    //Parse the JSON and check the results
-    JsonNode root = key.length() == 0 ? JsonConverter.parse(result) : JsonConverter.parse(result, key);
-    logger.debug("response object count {}", root.size());
-    Assert.assertTrue(root.size() == 10);
   }
 }
